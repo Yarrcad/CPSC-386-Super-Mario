057520fd234e61787f1cac87b3b87239d7cd2cac
Yarrcad <jonathancardinale@gmail.com>
2018-11-07 18:19:28 -0800

    asd

---
+++ SuperMario1-1 - Copy/settings.py
@@ -0,0 +1,36 @@
+class Settings:
+    """A class to store all settings for Super Mario."""
+
+    def __init__(self):
+        """Initialize the game's static settings."""
+        # Screen settings.
+        # self.screen_width = TBD
+        # self.screen_height = TBD
+
+        # Mario settings
+        # self.mario_lives = 3
+        # self.mario_default_speed = TDB
+        # self.mario_max_speed = TDB
+        # self.mario_speedup_scale = TDB
+
+        # Goomba settings
+        # self.goomba_speed = TDB
+
+        # Koopa settings
+        # self.koopa_speed = TDB
+
+        # Piranha Plant settings
+        # self.pplant_speed = TDB
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        # Dynamic Mario settings
+
+        # Dynamic Goomba settings
+        # self.goomba_direction = 1
+
+        # Dynamic Koopa settings
+        # self.koopa_direction = 1
+
+        # Dynamic Piranha Plant settings
+        # self.pplant_direction = 1
